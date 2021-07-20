3ae7aaab25683d51082fc6ed61c875ac8740d41f
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:36:14 +0200

    'Update'

--- HelloWorld/scripts/HelloWorld.lua
+++ HelloWorld/scripts/HelloWorld.lua
@@ -1,20 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  HelloWorld
-  
-  Summary:
-  Introduction to lua scripting. Setting a periodic timer and printing "Hello World".
-                                                                              
-  Description:
-  This script is creating a periodic timer and printing "Hello World from script"
-  with an incrementing number on the debug console with every timer expiration.
-  
-  How to run:
-  To demo this script, it must be loaded to any device or the emulator. The output
-  can be seen at the console.
-  
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Have a counter for an increasing number inside the timer function
